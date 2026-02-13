1abf77ca0f3ff2cd8e7aa7c0a8cf252c6b43fc27
Omphemetse Digoamaje <ompheedigoamaje@gmail.com>
2026-02-13 09:22:26 +0200

    Add project files.

---
+++ Parcel_Delivery_System/Models/Parcel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcel_Delivery_System.Models
+{
+	public class Parcel
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string? TrackingNumber { get; set; }
+
+		[Required]
+		public string? SenderName { get; set; }
+
+		[Required]
+		public string? ReceiverName { get; set; }
+
+		public string? Status { get; set; }
+	}
+}
